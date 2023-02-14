6f1900a4073129ba6b365cbae03a2701261ad86c
Gagas Praharsa Bahar <gagaspbahar@gmail.com>
2023-02-14 22:28:44 +0700

    add click listener

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 // Main entry point for the application
+var lines = []
+var squares = []
+var rectangles = []
+var polygon = []
 
 
 // Initialize the WebGL context
@@ -8,6 +12,7 @@ if (!gl) {
     alert('WebGL not available');
 }
 
+
 // Create, upload, and compile the shaders
 var vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
 var fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource);
@@ -16,20 +21,6 @@ function randomInt(range) {
     return Math.floor(Math.random() * range);
 }
 
-function setRectangle(gl, x, y, width, height) {
-    var x1 = x;
-    var x2 = x + width;
-    var y1 = y;
-    var y2 = y + height;
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-        x1, y1,
-        x2, y1,
-        x1, y2,
-        x1, y2,
-        x2, y1,
-        x2, y2,
-    ]), gl.STATIC_DRAW);
-}
 
 // Link the two shaders above into a program
 var program = createProgram(gl, vertexShader, fragmentShader);
@@ -43,6 +34,15 @@ var positionBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
 gl.viewport(0, 0, canvas.width, canvas.height);
+
+canvas.addEventListener("click", function (event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    setRectangle(gl, x, y, 50, 50);
+    gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
+    gl.drawArrays(gl.TRIANGLES, 0, 6);
+    // drawcanvas()
+}, false)
 
 var size = 2;          // 2 components per iteration
 var type = gl.FLOAT;   // the data is 32bit floats
@@ -59,31 +59,46 @@ gl.enableVertexAttribArray(positionAttributeLocation);
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
 setRectangle(gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300));
-    
-    
+
+
 // Tell WebGL how to convert from clip space to pixels
 gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
 
 function drawcanvas() {
+    var primitiveType;
+    var offset;
+    var count;
     gl.clearColor(0, 0, 0, 0);
     gl.clear(gl.COLOR_BUFFER_BIT);
     
     gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height); 
-    var positions = [
-        10, 20,
-        80, 20,
-        10, 30,
-        10, 30,
-        80, 20,
-        80, 30,
-    ]
     
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-    gl.uniform4f(colorUniformLocation, 100, 100, 100, 1);
-    var primitiveType = gl.TRIANGLES;
-    var offset = 0;
-    var count = 6;
-    gl.drawArrays(primitiveType, offset, count);
+
+    // Draw the garis.
+    if (lines.length != 0) {
+        primitiveType = gl.LINES;
+        offset = 0;
+        count = 2;
+        for (var i = 0; i < lines.length; i++) {
+            createGaris(gl, lines[i].x, lines[i].y, lines[i].width, lines[i].height)
+            gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
+            gl.drawArrays(primitiveType, offset, count);
+        }
+    }
+
+    // Draw the rectangle.
+
+    if (rectangles.length != 0) {
+        primitiveType = gl.TRIANGLES;
+        offset = 0;
+        count = 6;
+        for (var i = 0; i < rectangles.length; i++) {
+            createRectangle(gl, rectangles[i].x, rectangles[i].y, rectangles[i].width, rectangles[i].height)
+            gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
+            gl.drawArrays(primitiveType, offset, count);
+        }
+    }
+
 }
 
-drawcanvas()
+// drawcanvas()
