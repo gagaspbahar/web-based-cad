319efdd8b27f958cc7fb3bc89270a20d3fb8052c
Gagas Praharsa Bahar <gagaspbahar@gmail.com>
2023-02-26 12:51:49 +0700

    refactor: move pure functions

--- line.js
+++
@@ -1,6 +0,0 @@
-function createLine(gl, x1, x2, y1, y2) {
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-    x1, y1,
-    x2, y2,
-  ]), gl.STATIC_DRAW);
-}
--- main.js
+++ main.js
@@ -33,8 +33,7 @@ var unionSelected = false;
 var intersectionSelected = false;
 var preserveShapeSelected = false;
 var polygonActionFlag = false;
-var currentColorMode = "shape";
-// var colorMode = "vertex";
+var currentColorMode = "shape"; // shape or vertex
 
 
 /*
@@ -181,14 +180,6 @@ const setShapeColor = () => {
   setHexToRgb(colorInput);
 };
 
-const getIndexById = (id) => {
-  for (var i = 0; i < shapes.length; i++) {
-    if (shapes[i].id == id) {
-      return i;
-    }
-  }
-};
-
 const changeTranslationMode = () => {
   const elmt = document.getElementById("input-slider-label");
   if (translationMode === "x") {
@@ -220,23 +211,6 @@ const preserveShape = () => {
       "Do Not Preserve Shape";
   }
 };
-
-const customSplice = (arr, pair) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == pair[0] && arr[i + 1] == pair[1]) {
-      arr.splice(i, 2);
-    }
-  }
-  return arr;
-};
-
-const getVertexIndex = (arr, pair) => {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] == pair[0] && arr[i + 1] == pair[1]) {
-      return i;
-    }
-  }
-}
 
 const execTranslation = () => {
   if (currentAction == "translation") {
@@ -277,18 +251,6 @@ const execRotation = () => {
   }
 };
 
-const resize = (shapeVertices) => {
-  const [x, y] = calculateMidPoint(shapeVertices);
-  const scale = sliderValue / 50;
-  const newVertices = shapeVertices.map((val, idx) => {
-    if (idx % 2 == 0) {
-      return scale * (val - x) + x;
-    } else {
-      return scale * (val - y) + y;
-    }
-  });
-  return newVertices;
-};
 
 const execDilatation = () => {
   if (currentAction == "dilatation") {
@@ -549,80 +511,7 @@ const execIntersectionShape = () => {
 };
 
 
-function isInsidePolygon(x, y, vertices) {
-  var inside = false;
-  for (var i = 0, j = vertices.length - 2; i < vertices.length; i += 2) {
-    var xi = vertices[i],
-      yi = vertices[i + 1];
-    var xj = vertices[j],
-      yj = vertices[j + 1];
 
-    var intersect =
-      ((yi > y) != (yj > y)) && (x < ((xj - xi) * (y - yi)) / (yj - yi) + xi);
-    if (intersect) inside = !inside;
-    j = i;
-  }
-  console.log("x: " + x + " y: " + y + " inside: " + inside);
-  return inside;
-}
-
-function getIntersection(x1, y1, x2, y2, x3, y3, x4, y4) {
-  var denominator = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
-  if (denominator == 0) {
-    return null;
-  }
-  var a = y1 - y3;
-  var b = x1 - x3;
-  var numerator1 = (x4 - x3) * a - (y4 - y3) * b;
-  var numerator2 = (x2 - x1) * a - (y2 - y1) * b;
-  a = numerator1 / denominator;
-  b = numerator2 / denominator;
-
-  // if we cast these lines infinitely in both directions, they intersect here:
-  var x = x1 + a * (x2 - x1);
-  var y = y1 + a * (y2 - y1);
-
-  // if line1 is a segment and line2 is infinite, they intersect if:
-  if (a > 0 && a < 1 && b > 0 && b < 1) {
-    return [x, y];
-  } else {
-    return null;
-  }
-}
-
-const calculateMidPoint = (vertices) => {
-  var x = 0;
-  var y = 0;
-  for (var i = 0; i < vertices.length; i += 2) {
-    x += vertices[i];
-    y += vertices[i + 1];
-  }
-  return [x / (vertices.length / 2), y / (vertices.length / 2)];
-};
-
-const scaleDegreeFrom100 = (x) => {
-  return (x * 360) / 100;
-};
-
-const angleInEighthRadians = (x) => {
-  return (x * Math.PI) / (180 * 8);
-};
-
-const scale100FromCanvasX = (x) => {
-  return (x * 100) / canvas.width;
-};
-
-const scaleCanvasFrom100X = (x) => {
-  return (x * canvas.width) / 100;
-};
-
-const scale100FromCanvasY = (y) => {
-  return (y * 100) / canvas.height;
-};
-
-const scaleCanvasFrom100Y = (y) => {
-  return (y * canvas.height) / 100;
-};
 
 const uncheckActionRadio = () => {
   var elements = document.getElementsByTagName("transformation");
@@ -684,8 +573,6 @@ const render = (type, vertices, color) => {
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(color), gl.STATIC_DRAW);
   gl.vertexAttribPointer(colorAttributeLocation, 4, gl.FLOAT, false, 0, 0);
   gl.enableVertexAttribArray(colorAttributeLocation);
-  // gl.attribute4f(colorAttributeLocation, ...color);
-  //   gl.uniform2f(rotationUniformLocation, rotation[0], rotation[1]);
   gl.drawArrays(type, 0, vertices.length / 2);
 };
 
@@ -701,54 +588,11 @@ const renderCornerPoint = (shape) => {
   }
 };
 
-const distance = (x1, y1, x2, y2) => {
-  return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
-};
-
-const findNearestPoint = (x, y) => {
-  var min = 1000000;
-  var index = 0;
-  var vertex = [];
-  for (var i = 0; i < shapes.length; i++) {
-    for (var j = 0; j < shapes[i].vertices.length; j += 2) {
-      var dist = distance(
-        x,
-        y,
-        shapes[i].vertices[j],
-        shapes[i].vertices[j + 1]
-      );
-      if (dist < min) {
-        min = dist;
-        vertex = [shapes[i].vertices[j], shapes[i].vertices[j + 1]];
-        index = shapes[i].id;
-      }
-    }
-  }
-  return {
-    vertex: vertex,
-    index: index,
-  };
-};
-
-const canvasX = (x) => {
-  const rect = canvas.getBoundingClientRect();
-  let newX = x - rect.left;
-  newX = (newX / (rect.right - rect.left)) * canvas.width;
-  return newX;
-};
-
-const canvasY = (y) => {
-  const rect = canvas.getBoundingClientRect();
-  let newY = y - rect.top;
-  newY = (newY / (rect.bottom - rect.top)) * canvas.height;
-  return newY;
-};
-
 canvas.addEventListener(
   "mousedown",
   function (event) {
-    var x = canvasX(event.clientX);
-    var y = canvasY(event.clientY);
+    var x = canvasX(canvas, event.clientX);
+    var y = canvasY(canvas, event.clientY);
     let x1, x2, y1, y2;
 
     setShapeColor();
@@ -983,8 +827,8 @@ canvas.addEventListener("mousemove", function (event) {
   setShapeColor();
   drawcanvas();
   if (isDrawing) {
-    let x2 = canvasX(event.clientX);
-    let y2 = canvasY(event.clientY);
+    let x2 = canvasX(canvas, event.clientX);
+    let y2 = canvasY(canvas, event.clientY);
     if (currentShape == "line") {
       temporaryLine = [temporaryLine[0], temporaryLine[1], x2, y2];
       render(gl.LINES, temporaryLine, [...colorRgb, ...colorRgb]);
@@ -1019,9 +863,18 @@ const clearCanvas = () => {
   shapes = [];
   temporaryLine = [];
   temporaryColor = [];
+  selectedShapeId2 = 0;
+  selectedVertex2 = [];
   selectedShapeId = 0;
   selectedVertex = 0;
+  isDrawing = false;
 
+  currentPolygonAction = "none";
+  sliderValue = 0;
+  unionSelected = false;
+  intersectionSelected = false;
+  preserveShapeSelected = false;
+  polygonActionFlag = false;
   drawcanvas();
 };
 
